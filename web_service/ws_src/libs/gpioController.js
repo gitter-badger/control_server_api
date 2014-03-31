37e1404a256d6db3c3809fa46acc21f1d4643e55
Rihan Pereira <rihen234@gmail.com>
2014-03-31 09:32:49 +0530

    added gpio control file

---
+++ web_service/ws_src/libs/gpioController.js
@@ -0,0 +1,19 @@
+/*
+ * gpioControlller.js - This file manages the execution of gpio headers
+ */
+
+/* jslint   node  : true , continue : true,
+ devel         : true , indent   : 2,    maxerr   : 50,
+ newcap        : true , nomen    : true, plusplus : true,
+ regexp        : true , sloppy   : true, vars     : false
+ */
+
+//--------------define module scope variables---------------------------
+
+
+//--------------end module scope variables------------------------------
+
+//--------------begin server configuration------------------------------
+
+
+//--------------end server configuration--------------------------------
