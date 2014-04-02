0fc049a108619650db32128b07c5279ab4052ace
Rihan Pereira <rihen234@gmail.com>
2014-04-02 17:14:49 +0530

    deleted old code

---
+++ web_service/ws_src/api.js
@@ -0,0 +1,101 @@
+/*
+ * api.js - This file manages the execution of gpio headers
+ */
+
+/* jslint   node  : true , continue : true,
+ devel         : true , indent   : 2,    maxerr   : 50,
+ newcap        : true , nomen    : true, plusplus : true,
+ regexp        : true , sloppy   : true, vars     : false
+ */
+
+//--------------define module scope variables---------------------------
+'use strict';
+var
+    gpio = require('pi-gpio'); //gives access to low-level physical gpio pins
+
+//--------------end module scope variables------------------------------
+
+//--------------begin server configuration------------------------------
+module.exports = (function() {
+    var
+        welcome = "Welcome! This is GPIO control center." +
+                          "Begin commanding" +" the define API.",
+        pinHolder = [11,12,13,15,16,18,22], //predined safe working gpio headers to use
+        validProperties = {
+            pin_no: /^[0-9]{2}$/,
+            direction: ['output','input']
+        },
+        logBadParameters = [ ],
+        validateParameters,
+        triggerPin,
+        pinlist = {
+            pin17: 11,
+            pin18: 12,
+            pin21: 13,
+            pin22: 15,
+            pin23: 16,
+            pin24: 18,
+            pin25: 22
+        },
+        report; //variable used to fill in result of action performed
+
+    validateParameters = function(pin_no, direction) {
+        var flagparameters = true;
+
+        if(validProperties.pin_no.test(pin_no) === false){
+            logBadParameters.push(pin_no);
+            flagparameters = false;
+        }
+
+        if(pinHolder.indexOf(pin_no) === -1){
+            logBadParameters.push(pin_no);
+            flagparameters = false;
+        }
+
+
+        if(!((direction === validProperties.direction[0]) ||
+            (direction === validProperties.direction[1])
+            )) {
+            logBadParameters.push(direction);
+            flagparameters = false;
+        }
+
+        return flagparameters;
+    };
+
+    triggerPin = function(pin, direction, value) {
+        if(validateParameters(pin, direction)) {
+            // use gpio module and trigger the pin
+            gpio.open(pin, direction, function(err){
+                gpio.write(pin, value, function(){
+                    gpio.close(pin);
+                });
+            });
+
+            //report the result in object literal
+            report = {
+                message: "success",
+                pin : pin_no,
+                direction: direction,
+                state: value
+            };
+
+        }
+        else {
+            report = {
+              message: "failed",
+              badparameters: logBadParameters
+            };
+        }
+        return report;
+    };
+
+    return {
+        greet: welcome,
+        triggerPin: triggerPin,
+        pinlist: pinlist,
+        pinHolder: pinHolder
+    };
+}());
+
+//--------------end server configuration--------------------------------
--- web_service/ws_src/routes/routes.js
+++ web_service/ws_src/routes/routes.js
@@ -10,10 +10,9 @@
 
 //-------define module scope variables----------------
 'use strict';
-var serviceGreeter = "Welcome! This is GPIO control center." +
-                        "Begin commanding" +" the define API.",
-    gpioController = require('../libs/gpioController'),
-    gpio = require('pi-gpio');
+var
+    gpioController = require('.././api'),
+    pin,direction,value;
 
 //-------end module scope variable declaration--------
 
@@ -21,12 +20,12 @@ var serviceGreeter = "Welcome! This is GPIO control center." +
 module.exports = function( app ) {
     app.get( '/',function  (req, res) {
         //a simple http server
-        res.setHeader( 'Content-length', serviceGreeter.length );
+        res.setHeader( 'Content-length', gpioController.greet.length );
         res.writeHead(200 ,{ 'Content-type' : 'text/plain' });
-        res.end( serviceGreeter );
-        if (serviceGreeter.length) {
+        res.end( gpioController.greet );
+        if (gpioController.greet.length) {
             //calculate and randamize string
-            console.log("length : " + " " + serviceGreeter.length);
+            console.log("length : " + " " + gpioController.greet.length);
             //console.log(process.env.NODE_ENV);
         }
     });
@@ -38,21 +37,25 @@ module.exports = function( app ) {
 
     app.get('/gpio/pinlist', function(req, res) {
             res.send({
-                pins: gpioController.pins
+                pinlist: gpioController.pinlist
             });
-        console.log("accessed pin list" + " "+ gpioController.pins);
+        console.log("accessed pin list" + " "+ gpioController.pinHolder);
     });
 
-    app.get('/gpio/:pin_no([0-9]+)/:direction((in|out))/:value((0|1))',
+    app.get('/gpio/:pin_no([0-9]+)/:direction/:state((0|1))',
         function(req, res) {
-            var pin = req.params.pin_no,
+            var
+                pin = req.params.pin_no,
                 direction = req.params.direction,
-                value = req.params.value;
+                value = req.params.state,
+                result;
 
+            result = gpioController.triggerPin(pin, direction, value);
+/*
             gpio.open(pin, direction, function(err){
                  gpio.write(pin, value, function(){
                      gpio.close(pin);
-                     var report = {
+                     report = {
                          message:"success",
                          pin: pin,
                          direction: direction,
@@ -62,9 +65,10 @@ module.exports = function( app ) {
                      console.log(report);
                      res.send(report);
                  });
-            });
+            }); */
+            res.send(result);
+            console.log(result);
     });
-
 
 };
 
